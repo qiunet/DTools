2ba0362eb4ce403d5ec0900aad32ba281089be59
huangjianping <huangjianping@newlang.cn>
2022-04-28 10:54:58 +0800

    sha1加密转换

--- src/renderer/src/common/CryptoUtil.ts
+++ src/renderer/src/common/CryptoUtil.ts
@@ -9,8 +9,27 @@ export class CryptoUtil {
      * @param context
      */
     public static md5(context: string): string {
-        let md5 = crypto.createHash('md5');
-        return md5.update(context, 'utf-8').digest('hex');
+        return this.encrypt('md5', context);
+    }
+
+    /**
+     * sha1加密
+     * @param context 加密内容
+     * @returns 
+     */
+    public static sha1(context: string): string {
+        return this.encrypt('sha1', context);
+    }
+
+    /**
+     * 加密
+     * @param algorithm 加密算法
+     * @param context   加密内容
+     * @returns 加密后字符串
+     */
+    public static encrypt(algorithm:string, context:string):string{
+        let hash = crypto.createHash(algorithm);
+        return hash.update(context, 'utf-8').digest('hex');
     }
 
 }
